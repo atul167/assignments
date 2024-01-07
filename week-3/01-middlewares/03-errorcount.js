a3ce94b8cebca6f858e5077e1b0db498979101f9
Atul Dwivedi <atuldwivedi1628@gmail.com>
2024-01-07 22:45:02 +0530

    week 3 assignment done

--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -1,14 +1,8 @@
 const request = require('supertest');
 const assert = require('assert');
 const express = require('express');
-
 const app = express();
 let errorCount = 0;
-
-// You have been given an express server which has a few endpoints.
-// Your task is to
-// 1. Ensure that if there is ever an exception, the end user sees a status code of 404
-// 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
 
 app.get('/user', function(req, res) {
   throw new Error("User not found");
@@ -22,5 +16,22 @@ app.post('/user', function(req, res) {
 app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
+// You have been given an express server which has a few endpoints.
+// Your task is to
+// 1. Ensure that if there is ever an exception, the end user sees a status code of 404
+// 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
 
+//Error handling middleware......
+// at the end of our file
+//Through this express will not send the common 500 status code ,but only what we want to send as error code
+function errorfinder(err, req, res, next) {
+  errorCount = errorCount + 1;
+  const statusCo = err.statusCode;
+  if (statusCo != 200) {
+    res.sendStatus(404);
+  }
+  else
+    next(err);
+}
+app.use(errorfinder);
 module.exports = app;
